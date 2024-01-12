9cf4af1e962f20f48ad1db621c0c380b0d7e89d4
aaksant <aaksangit@gmail.com>
2024-01-12 21:20:58 +0700

    Add initial add book functionality

--- main.js
+++ main.js
@@ -1,21 +1,67 @@
+let books = [];
+
+function Book(title, author, numOfPages) {
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+}
+
+function addBook() {
+  const newBook = getBookData();
+
+  if (!isAlreadyExist(newBook)) {
+    books.push(newBook);
+    clearInput();
+  }
+}
+
+function isAlreadyExist(book) {
+  return books.some((entry) => entry.title === book.title);
+}
+
+function getBookData() {
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const numOfPages = numOfPagesInput.value;
+
+  return new Book(title, author, numOfPages);
+}
+
+function clearInput() {
+  titleInput.value = '';
+  authorInput.value = '';
+  numOfPagesInput.value = '';
+}
+
+// Inputs
+let titleInput = document.getElementById('title');
+let authorInput = document.getElementById('author');
+let numOfPagesInput = document.getElementById('numOfPages');
+
 // Buttons
 const addButton = document.getElementById('add-btn');
+const addForm = document.getElementById('add-form');
+const confirmAdd = document.getElementById('confirm-add');
 
 // Dialog
 const dialog = document.getElementById('dialog');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const numOfPages = document.getElementById('numOfPages');
 
-// Add book button handling
+// Add book button
 addButton.addEventListener('click', () => dialog.showModal());
 
-dialog.addEventListener('close', () => {
-  title.value = '';
-  author.value = '';
-  numOfPages.value = '';
+document.addEventListener('click', (e) => {
+  if (e.target === dialog) {
+    clearInput();
+    dialog.close();
+  }
 });
 
-document.addEventListener('click', (event) => {
-  if (event.target === dialog) dialog.close();
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    clearInput();
+    dialog.close();
+  }
 });
+
+// Confirm add
+addForm.onsubmit = addBook;
